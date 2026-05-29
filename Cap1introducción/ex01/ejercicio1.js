5a69815998416e46f3bcb1521d5b652163349a39
valencialaura170-bit <valencialaura170@gmail.com>
2026-05-28 20:22:46 -0500

    subierndo archivos nuevos

--- Cap1introducción/ex01/ejercicio1.js
+++ Cap1introducción/ex01/ejercicio1.js
@@ -1,2 +1,10 @@
 /*
+Esto sirve para realizar comentarios teoricos
+Los cometarios son utiles para explicar el proposito de ciertas partes del codigo, para dejar notas para otros desarrolladores o para algo importante sobre el codigo en el futuro.
+*/
+
+//Esto es un comentario de una sola linea, se utiliza para escribir comentarios breves o para comentar una sola linea de codigo.
+
+document.write("Hola Mundo");
+console.log("Hola desde la comsola");
 
